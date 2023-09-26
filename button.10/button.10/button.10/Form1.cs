b752e82fff8c2c865e7974142ff3779931c78ccb
11meryem <145452149+11meryem@users.noreply.github.com>
2023-09-26 16:01:55 +0300

    Add files via upload

---
+++ button.10/button.10/button.10/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float yazili1, yazili2, ortalama;
+            yazili1 = Convert.ToSingle(txtyazili1.Text);
+            yazili2 = Convert.ToSingle(txtyazili2.Text);
+            ortalama=(yazili1+yazili2)/ 2;
+            txtortalama.Text = ortalama.ToString();
+        }
+    }
+}
